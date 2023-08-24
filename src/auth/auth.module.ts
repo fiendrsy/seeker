350fdceaeed997e838da31003c12999e1a79bc2d
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2023-08-24 15:02:19 +0300

    fixed the paths

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,8 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UsersModule } from "../users/users.module";
-import { AuthTokensService } from "./auth.tokens.service";
-import { AuthValidationService } from "./auth.validation.service";
+import { AuthTokensService } from "./auth-tokens.service";
+import { AuthValidationService } from "./auth-validation.service";
 
 @Module({
   imports: [UsersModule, JwtModule],
