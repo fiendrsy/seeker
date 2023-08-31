d1c6db4e7d64a37dd094820d14a9ca546e19ec6c
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2023-08-31 10:32:30 +0300

    code refactor

--- src/guards/helper/find-bearer.ts
+++ src/guards/helper/find-bearer.ts
@@ -3,10 +3,8 @@ import { HttpException, HttpStatus } from "@nestjs/common";
 import { NOT_AUTHORIZED } from "../constants";
 
 export function getBearerToken(req: Request): string {
-  const authHeader = req.headers.authorization;
-  const bearer = authHeader?.split(" ")[0];
-  const token = authHeader?.split(" ")[1];
-  if (bearer !== "Bearer" || !token) {
+  const [type, token] = req.headers.authorization?.split(" ") ?? [];
+  if (type !== "Bearer" || !token) {
     throw new HttpException(NOT_AUTHORIZED, HttpStatus.UNAUTHORIZED);
   }
   return token;
